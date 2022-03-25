46e562a75c8d270f0c450b274e3cd3f881c914bc
Fernando Majin Sampayo  Perez <102370094+fernandospm@users.noreply.github.com>
2022-03-25 15:04:50 +0000

    componentes

--- sokoban.py
+++ sokoban.py
@@ -1,1 +1,15 @@
-print("hola")
+class sokoban:
+ """
+  definir los componentes
+  
+  0 - personaje
+  1 - espacio
+  2 - caja
+  3 - pared
+  4 - meta
+  5 - personaje_meta
+  6 - caja_meta
+  """
+  
+  
+  
