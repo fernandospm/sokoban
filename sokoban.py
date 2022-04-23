f312e9503e6edb9e2451769daf60212751f40058
Fernando Majin Sampayo  Perez <102370094+fernandospm@users.noreply.github.com>
2022-04-23 05:22:11 +0000

    posesion

--- sokoban.py
+++ sokoban.py
@@ -10,31 +10,52 @@ class sokoban:
   5 - personaje_meta
   6 - caja_meta
   """
+ posicion_columna = 4
+ mapa = [1,3,3,3,2,3,3,3,1]
 
-mapa = [
- [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
- [1,3,3,1,3,3,3,3,3,3,3,3,3,3,3,1,1,1,1,1],
- [1,3,3,1,3,2,3,3,3,3,3,3,0,3,3,1,1,1,1,1],
- [1,3,0,1,3,3,3,0,3,3,3,3,3,3,3,3,3,3,3,1],
- [1,3,3,0,3,3,3,0,3,3,3,3,3,3,3,3,4,4,4,1],
- [1,3,3,3,3,3,3,0,3,3,3,3,3,3,3,3,4,4,4,1],
- [1,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,4,4,4,1],
- [1,3,3,0,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,1],
- [1,3,3,0,3,3,3,3,3,3,0,3,3,3,3,1,1,1,1,1],
- [1,3,3,3,3,3,3,3,3,3,3,3,3,3,3,1,1,1,1,1],
- [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
- ]
+ def __init__(self):
+  pass #
 
-muneco_fila = 2
-muneco_columna = 5
+ def imprimirMapa(self):
+     for i in self.mapa:
+         if i == 3:
+             print(" ", end = "")
+         elif i == 2:
+            print(chr(64), end = "")
+         elif i == 1:
+            print("|", end = "")
+         else:
+           print(i, end = "")
+     print()
 
-def imprimirMapa(self):
-  for fila in self.mapa:
-   print(fila)
+ def moverDerecha(self):
+   self.posicion_columna += 1
+   self.mapa[self.posicion_columna] = 2
+   self.mapa[self.posicion_columna + 1] = 3
+   self.imprimirMapa()
+  
+ def moverIzquierda(self):
+   self.posicion_columna -= 1
+   self.mapa[self.posicion_columna] = 2
+   self.mapa[self.posicion_columna - 1] = 3
+   self.imprimirMapa()
     
-def jugar(self):
- self.imprimirMapa()
+juego = sokoban()
+juego.imprimirMapa()
 
+instrucciones = "q-Salir\nd-Derecha\na-Izquierda"
+
+while True:
+  print(instrucciones)
+  movimiento = input("Mover: ")
+  if movimiento == "d":
+    juego.moverDerecha()
+  elif movimiento == "a":
+    juego.moverIzquierda()
+  elif movimiento == "q":
+    print("saliste del juego")
+    break
+  
 def moverDerecha(self):
 # 0 (personaje, espacio)
   if (
@@ -115,27 +136,27 @@ def moverDerecha(self):
             self.mapa[self.personaje_fila][self.personaje_columna + 1] = 5
             self.personaje_columna += 1  
           
- # 8 (personaje_meta, caja, espacio)
+# 8 (personaje_meta, caja, espacio)
   elif (
-            self.mapa[self.personaje_fila][self.personaje_columna] == 5
-            and self.mapa[self.personaje_fila][self.personaje_columna + 1] == 2
-            and self.mapa[self.personaje_fila][self.personaje_columna + 2] == 1
+        self.mapa[self.personaje_fila]  [self.personaje_columna] == 5
+        and self.mapa[self.personaje_fila][self.personaje_columna + 1] == 2
+        and self.mapa[self.personaje_fila][self.personaje_columna + 2] == 1
    ):
-            self.mapa[self.personaje_fila][self.personaje_columna] = 4
-            self.mapa[self.personaje_fila][self.personaje_columna + 1] = 0
-            self.mapa[self.personaje_fila][self.personaje_columna + 2] = 2
-            self.personaje_columna += 1  
+        self.mapa[self.personaje_fila][self.personaje_columna] = 4
+        self.mapa[self.personaje_fila][self.personaje_columna + 1] = 0
+        self.mapa[self.personaje_fila][self.personaje_columna + 2] = 2
+        self.personaje_columna += 1  
           
 # 9 (personaje_meta, caja, meta)
   elif (
-            self.mapa[self.personaje_fila][self.personaje_columna] == 5
-            and self.mapa[self.personaje_fila][self.personaje_columna + 1] == 2
-            and self.mapa[self.personaje_fila][self.personaje_columna + 2] == 4
+        self.mapa[self.personaje_fila][self.personaje_columna] == 5
+        and self.mapa[self.personaje_fila][self.personaje_columna + 1] == 2
+        and self.mapa[self.personaje_fila][self.personaje_columna + 2] == 4
   ):
-            self.mapa[self.personaje_fila][self.personaje_columna] = 4
-            self.mapa[self.personaje_fila][self.personaje_columna + 1] = 0
-            self.mapa[self.personaje_fila][self.personaje_columna + 2] = 6
-            self.personaje_columna += 1  
+        self.mapa[self.personaje_fila][self.personaje_columna] = 4
+        self.mapa[self.personaje_fila][self.personaje_columna + 1] = 0
+        self.mapa[self.personaje_fila][self.personaje_columna + 2] = 6
+        self.personaje_columna += 1  
 
 # 10 (personaje_meta, caja_meta, espacio)
   elif (
@@ -149,16 +170,16 @@ def moverDerecha(self):
             self.personaje_columna += 1  
 # 11 (personaje_meta, caja_meta_, meta)
   elif (
-            self.mapa[self.personaje_fila][self.personaje_columna] == 5
-            and self.mapa[self.personaje_fila][self.personaje_columna + 1] == 6
-            and self.mapa[self.personaje_fila][self.personaje_columna + 2] == 4
+          self.mapa[self.personaje_fila][self.personaje_columna] == 5
+          and self.mapa[self.personaje_fila][self.personaje_columna + 1] == 6
+          and self.mapa[self.personaje_fila][self.personaje_columna + 2] == 4
   ):
-            self.mapa[self.personaje_fila][self.personaje_columna] = 4
-            self.mapa[self.personaje_fila][self.personaje_columna + 1] = 5
-            self.mapa[self.personaje_fila][self.personaje_columna + 2] = 6
-            self.personaje_columna += 1  
-
-
+          self.mapa[self.personaje_fila][self.personaje_columna] = 4
+          self.mapa[self.personaje_fila][self.personaje_columna + 1] = 5
+          self.mapa[self.personaje_fila][self.personaje_columna + 2] = 6
+          self.personaje_columna += 1   
+  
+  
   
   
 
