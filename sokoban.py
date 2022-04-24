c13ecccebedc17734cfbb3cecd77652939b32e3e
Fernando Majin Sampayo  Perez <102370094+fernandospm@users.noreply.github.com>
2022-04-24 01:51:52 +0000

    Izquierda 4,5,6

--- sokoban.py
+++ sokoban.py
@@ -184,6 +184,43 @@ def moverIzquierda(self):
           self.mapa[self.personaje_fila][self.personaje_columna - 2] = 2
           self.personaje_columna -= 1
           print("# 2 (personaje, caja, espacio) izquierda")
+
+# 3 (personaje, caja, meta)
+    elif (
+          self.mapa[self.personaje_fila][self.personaje_columna] == 0
+          and self.mapa[self.personaje_fila][self.personaje_columna - 1] == 2
+          and self.mapa[self.personaje_fila][self.personaje_columna - 2] == 4
+    ):
+          self.mapa[self.personaje_fila][self.personaje_columna] = 1
+          self.mapa[self.personaje_fila][self.personaje_columna - 1] = 0
+          self.mapa[self.personaje_fila][self.personaje_columna - 2] = 6
+          self.personaje_columna -= 1
+          print("# 3 (personaje, caja, meta) izquierda")
+
+# 4 (personaje, caja_meta, espacio)
+    elif (
+          self.mapa[self.personaje_fila][self.personaje_columna] == 0
+          and self.mapa[self.personaje_fila][self.personaje_columna - 1] == 6
+          and self.mapa[self.personaje_fila][self.personaje_columna - 2] == 1
+    ):
+          self.mapa[self.personaje_fila][self.personaje_columna] = 1
+          self.mapa[self.personaje_fila][self.personaje_columna - 1] = 5
+          self.mapa[self.personaje_fila][self.personaje_columna - 2] = 2
+          self.persoanje_columna -= 1
+          print("# 4 (personaje, caja_meta, espacio) izquierda")
+
+# 5 (personaje, caja_meta, meta)
+    elif (
+          self.mapa[self.personaje_fila][self.personaje_columna] == 0
+          and self.mapa[self.personaje_fila][self.personaje_columna - 1] == 6
+          and self.mapa[self.personaje_fila][self.personaje_columna - 2] == 4
+    ):
+          self.mapa[self.personaje_fila][self.personaje_columna] = 1
+          self.mapa[self.personaje_fila][self.personaje_columna - 1] = 5
+          self.mapa[self.personaje_fila][self.personaje_columna - 2] = 6
+          self.personaje_columna -= 1
+          print("# 5 (personaje, caja_meta, meta) izquierda")
+
   
     
 juego = sokoban()
